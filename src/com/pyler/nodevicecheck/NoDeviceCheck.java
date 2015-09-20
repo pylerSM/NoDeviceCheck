81a775b3c96aa2e0449540cb0e4fb8bf82bf33d1
pyler <pyler@azet.sk>
2015-09-20 13:32:01 +0200

    Do not check root and selinux state

--- src/com/pyler/nodevicecheck/NoDeviceCheck.java
+++ src/com/pyler/nodevicecheck/NoDeviceCheck.java
@@ -10,6 +10,26 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class NoDeviceCheck implements IXposedHookLoadPackage {
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+		if ("android".equals(lpparam.packageName)) {
+            		XposedHelpers.findAndHookMethod(File.class, "exists",
+                    		new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param)
+                                throws Throwable {
+                            File file = (File) param.thisObject;
+                            if (new File("/sys/fs/selinux/enforce").equals(file)) {
+                                param.setResult(true);
+                                return;
+                            }
+
+                            if (new File("/system/bin/su").equals(file) || new File("/system/xbin/su").equals(file)) {
+                                param.setResult(false);
+                                return;
+                            }
+                        }
+                    });
+		}
+		
 		XposedHelpers.findAndHookMethod(JSONObject.class, "getBoolean",
 				String.class, new XC_MethodHook() {
 					@Override
